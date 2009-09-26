6a28b56b54afa024ad1674832af35f13491a435c
dudu <complex857@gmail.com>
2009-09-26 21:29:56 +0200

    delete moved files

--- constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('STORAGE_PATH', './pic/');
-define('TMP_PATH', '/tmp/');
-define('DB_PATH', './list.csv');
-define('THUMB_PATH', './thumb/');
-define('THUMB_WIDTH', 200);
-define('THUMB_HEIGHT', 200);
-define('PAGE_LIMIT', 18);
