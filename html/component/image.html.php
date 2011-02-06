b0735f4c1364dd56edc9f0ff64dca16ec8c12736
dudu <complex857@gmail.com>
2011-02-06 20:57:20 +0100

    css fiddle

--- html/component/image.html.php
+++ html/component/image.html.php
@@ -1,11 +1,12 @@
 <div id="image_<?php print $item->id?>" class="image <?php if (isset($item_i) && $item_i === 0) print "page_start"; if (isset($item_i) && $item_i === $limit - 1) print "page_end" ?>" data-id="<?php print $item->id?>" data-ctime="<?php print substr($item->ctime, 0, 10)?>" data-page="<?php print $page?>">
 
+	<span class="thumb_container">
 	<?php if (!$item->deleted): ?>
 		<a class="thumb" href="<?php print preg_replace('#/+#', '/', $item->path); ?>"><img alt="" src="<?php print $item->html_thumb(); ?>"/></a>
 	<?php else: ?>
 		<a class="thumb" href="<?php print $item->original_url; ?>"><img alt="" src="<?php print $item->html_thumb(); ?>"/></a>
 	<?php endif; ?>
-
+	</span>
 	<div>
 		<a href="<?php print '?'.http_build_query(array_merge($urlparams, array('nick' => $item->nick, 'page' => null))) ?>" class="nick"><?php print htmlspecialchars($item->nick); ?></a>
 		<span class="hide">hide</span>
